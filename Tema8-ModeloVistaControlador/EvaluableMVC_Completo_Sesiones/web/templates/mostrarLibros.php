2b8abedba04137e8fb539fa4bcfa8cab3f029153
Ivan Soriano <ivansoriano1901@gmail.com>
2023-01-25 11:52:15 +0100

    apuntes clase

---
+++ Tema8-ModeloVistaControlador/EvaluableMVC_Completo_Sesiones/web/templates/mostrarLibros.php
@@ -0,0 +1,18 @@
+<?php ob_start() ?>
+
+<table>
+	<tr>
+		<th><h4><b>Títulos</b></h4><br></th>		
+	</tr>
+	
+	<?php foreach ($params['libros'] as $libro) :?>
+	<tr>
+		<td><a href="index.php?ctl=verLibro&idLibro=<?php echo $libro['idLibro']?>" class="tablaP">
+		<?php echo $libro['titulo'] ?></a></td>
+	</tr>
+	<?php endforeach; ?>
+</table>
+
+<?php $contenido = ob_get_clean() ?>
+
+<?php include 'layout.php' ?>
